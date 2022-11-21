770668aa8223538849fc744ea0b85dd8018ad13d
sahilm-10 <malgundkarsahil10@gmail.com>
2022-11-21 13:15:13 +0530

    Added Queries and Params

--- methods.js
+++ methods.js
@@ -2,7 +2,35 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-let users = {};
+let users = [{
+    id:"1",
+    name:"Sahil",
+    age:"20"
+},
+{
+    id:"2",
+    name:"Rahul",
+    age:"30"
+},
+{
+    id:"3",
+    name:"Sam",
+    age:"24"
+},
+];
+
+// with query
+app.get("/users",(req,res)=>{
+    console.log(req.query);
+    // res.send(req.query);
+    let {name,age} = req.query;
+    let dataFiltered = users.filter(UserObj=> {
+        return(UserObj.name == name && UserObj.age == age);
+    });
+    res.send(dataFiltered);
+})
+
+
 app.get("/users",(req,res)=>{
     res.send(users);
 })
@@ -33,5 +61,19 @@ app.delete("/user",(req,res)=>{
         msg:"Data has been deleted!"
     })
 })
+
+// Params ***************
+app.get("/users/:id",(req,res)=>{
+    console.log(req.params.name);
+    // // for db
+    // let {id} = req.params;
+    // let user = db.findOne(id);
+    res.json({
+        msg:"User id:" ,
+        "obj" :req.params
+    });
+})
+
+
 
 app.listen(5000);
